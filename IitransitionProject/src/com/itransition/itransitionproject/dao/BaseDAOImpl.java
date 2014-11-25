e336ce70909862cf8549666648f28be4a610090a
Tatiana <radkevich.t.s@gmail.com>
2014-11-25 19:49:28 -0300

    added session-checker-2

--- IitransitionProject/src/com/itransition/itransitionproject/dao/BaseDAOImpl.java
+++ IitransitionProject/src/com/itransition/itransitionproject/dao/BaseDAOImpl.java
@@ -6,70 +6,53 @@ import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.itransition.itransitionproject.dao.interfaces.BaseDAO;
 
-@Transactional
 public class BaseDAOImpl implements BaseDAO {
 
 	private Session session;
-	
 	@Autowired
 	private SessionFactory sessionFactory ;
 	
 	@Override
-	@Transactional
 	public Object getRecordById(Class classObject, Integer id) {
-		checkSession();
-		Object obj = session.get(classObject, id);
+		Object obj = getSession().get(classObject, id);
 		return obj;
 	}
 
 	@Override
-	@Transactional
 	public void addElement(Object object) {
-		checkSession();
-		session.save(object);
+		getSession().save(object);
 	}
 
 	@Override
-	@Transactional
 	public <T> List<T> getAllRecords(String queryStr) {
-		checkSession();
-		List<T> list = session.createQuery(queryStr).list();
+		List<T> list = getSession().createQuery(queryStr).list();
 		return list;
 	}
 	
 	@Override
-	@Transactional
 	public void updateObject(Object object) {
-		checkSession();
-		session.update(object);
+		getSession().update(object);
 	}
 	
 	@Override
-	@Transactional
 	public void removeObject(Class classObject, Integer id) {
 		Object obj = getRecordById(classObject, id);
         if (null != obj) {
-        	checkSession();
-            session.delete(obj);
+        	getSession().delete(obj);
         }
 	}
 	
 	@Override
-	@Transactional
 	public void executeQuery(String query) {
-		checkSession();
-		session.createQuery(query).executeUpdate();
+		getSession().createQuery(query).executeUpdate();
 	}
 	
 	@Override
-	@Transactional
 	public Object getByQuery(String string, String nameParameter, Object parameter) {
-		checkSession();
-		Query query = session.createQuery(string);
+		Query query = getSession().createQuery(string);
 		query.setParameter(nameParameter, parameter);
 		List<Object> list = query.list();
 		if (null == list || list.isEmpty())
@@ -85,8 +68,9 @@ public class BaseDAOImpl implements BaseDAO {
 		this.sessionFactory = sessionFactory;
 	}
 	
-	private void checkSession() {
-		if (null == session || !session.isOpen() || !session.isConnected())
-			session = sessionFactory.openSession();
+	private Session getSession() {
+		if ( session == null || !session.isOpen() || !session.isConnected())//{
+			session = getSessionFactory().openSession();
+		return session;
 	}
 }
